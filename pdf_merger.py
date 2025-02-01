c1fe38940268580733aa347a915a41599b011d7b
Matthew Tong <mtong0722@gmail.com>
2025-01-31 21:44:10 -0500

    removed unnecessary print statement

--- pdf_merger.py
+++ pdf_merger.py
@@ -2,11 +2,10 @@ import PyPDF2
 import sys
 import os
 
-merger = PyPDF2.PdfMerger()
+merger = PyPDF2.PdfFileMerger()
 
 for file in os.listdir(os.curdir): 
     if file.endswith(".pdf"):   
-        print(file)
         merger.append(file)
     
 merger.write("combinedPDF.pdf")
